95cc5144c56e27af68a329b584fc4967f326a54b
Ammerix <emma.marqueton@gmail.com>
2022-03-28 19:49:37 +0200

    Ajout Composant search bar + DressingScreen

--- navigation/app-stacks.tsx
+++ navigation/app-stacks.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { createStackNavigator } from "@react-navigation/stack";
-import AuthenticationFormScreen from "../screens/AuthenticationFormScreen";
 import HomeScreen from "../screens/HomeScreen";
+import DressingScreen from "../screens/DressingScreen";
+
+
+
+
---
+++ screens/DressingScreen.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
+import SearchBar from "../components/searchBar";
+//import { DressingScreenProps } from "../navigation/app-stacks";
+//import ClothesDbApi from "../services/clothesdbapi.service";
+
+
+interface DressingScreenState {
+    Dressings: Array<Clothes>;
+}
+
+export default class DressingScreen extends Component<
+    DressingScreenProps,
+    DressingScreenState
+> {
+    state: DressingScreenState = {
+        Dressings: [],
+    };
+
+    getResults = (name: string) => {
+        ClothesVDbApi.searchDressingsByName(name).then((results) => {
+            this.setState({ Dressings: results });
+        });
+    };
+
+    render() {
+        const { navigation } = this.props;
+
+        return (
+            <View style={styles.container}>
+                <SearchBar onSubmitEditing={this.getResults} />
+                <ClothesList
+                    Clothes={this.state.Clothes}
+                    navigation={navigation}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
