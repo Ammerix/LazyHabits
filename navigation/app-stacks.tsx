d285b6b550c0f46a5108e2335b495a98d8779d3e
Ammerix <emma.marqueton@gmail.com>
2022-03-28 20:49:07 +0200

    Update app-stacks.tsx
    Ajout des stacks de Home et Dressing

--- navigation/app-stacks.tsx
+++ navigation/app-stacks.tsx
@@ -4,6 +4,30 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import DressingScreen from "../screens/DressingScreen";
 
+// Define view names and associated params
+// undefined = no params passed to view
+export type RootStackParamList = {
+    Home: undefined;
+    Dressing: undefined;
+};
+
+// Define view stack inside home tab
+const HomeStack = createStackNavigator<RootStackParamList>();
+export const HomeStackScreen = () => {
+    return (
+        // definition d'une pile, on est à home et on peut aller au dressing
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Home" component={HomeScreen} />
+            <HomeStack.Screen name="Dressing" component={DressingScreen} />
+        </HomeStack.Navigator>
+    );
+};
+
+export interface HomeScreenProps {
+    navigation: StackNavigationProp<RootStackParamList, "Home">;
+}
 
 
-
+export interface DressingScreenProps {
+    navigation: StackNavigationProp<RootStackParamList, "Dressing">;
+}
